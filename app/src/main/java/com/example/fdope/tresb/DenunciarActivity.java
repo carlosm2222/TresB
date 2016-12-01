031787293ff75a39b2266f152f1669fc33d5abab
fdoperezr <fdoperezr@outlook.com>
2016-12-01 02:19:51 -0300

    Denuncia terminado pero retorna false siempre

--- app/src/main/java/com/example/fdope/tresb/DenunciarActivity.java
+++ app/src/main/java/com/example/fdope/tresb/DenunciarActivity.java
@@ -1,5 +1,6 @@
 package com.example.fdope.tresb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.fdope.tresb.DB.ConsultasProductos;
 
@@ -17,81 +19,54 @@ import static com.example.fdope.tresb.R.id.motivo4;
 
 public class DenunciarActivity extends AppCompatActivity {
     private Button denunciar;
-    private  EditText inputMotivo;
     private Button b;
     private int idEvento;
     private RadioGroup radio;
     private RadioButton radioBoton;
-    private TextWatcher mTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-        }
 
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-            // check Fields For Empty Values
-            checkFieldsForEmptyValues();
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denunciar);
-        inputMotivo = (EditText) findViewById(R.id.motivo);
-        inputMotivo.setEnabled(false);
+        Bundle bundle = getIntent().getExtras();
+        idEvento = bundle.getInt("idEvento");
         b = (Button) findViewById(R.id.Denunciar);
-        b.setOnClickListener(new View.OnClickListener() {
+        radio = (RadioGroup) findViewById(R.id.radio);
+        b.setEnabled(false);
+        radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Bundle bundle = getIntent().getExtras();
-                idEvento = bundle.getInt("idEvento");
-                String motivo = (String) radioBoton.getText();
-                ConsultasProductos.agregarDenuncia(idEvento,motivo);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                b.setEnabled(true);
             }
         });
-
-        checkFieldsForEmptyValues();
         addListenerOnButton();
 
     }
 
     public void addListenerOnButton() {
-
-        radio = (RadioGroup) findViewById(R.id.radio);
-        denunciar = (Button) findViewById(R.id.Denunciar);
-
-        denunciar.setOnClickListener(new View.OnClickListener() {
+        b.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
                 // get selected radio button from radioGroup
                 int selectedId = radio.getCheckedRadioButtonId();
-                if (selectedId==motivo4){
-                    inputMotivo.setEnabled(true);
-                    checkFieldsForEmptyValues();
-                } else
-                    b.setEnabled(true);
+
                 // find the radiobutton by returned id
                 radioBoton = (RadioButton) findViewById(selectedId);
 
+                if (ConsultasProductos.agregarDenuncia(idEvento,(String)radioBoton.getText())==true) {
+                    Toast.makeText(DenunciarActivity.this, "Denuncia enviada", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else if (ConsultasProductos.agregarDenuncia(idEvento,(String)radioBoton.getText())==false){
+                    Toast.makeText(DenunciarActivity.this, "No se pudo enviar su denuncia.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
 
             }
 
         });
 
-    }
-
-    public void checkFieldsForEmptyValues() {
-
-        if (inputMotivo.equals("")) {
-            b.setEnabled(false);
-        } else {
-            b.setEnabled(true);
-        }
     }
 }
