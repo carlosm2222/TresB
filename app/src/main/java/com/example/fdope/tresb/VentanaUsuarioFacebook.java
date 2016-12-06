f0bfc25f9cdcabe1d511883fab85c134cb61e3d4
leandro <metallica_kirk62@hotmail.com>
2016-12-06 00:51:31 -0400

    fix boton ingresar por facebook

--- app/src/main/java/com/example/fdope/tresb/VentanaUsuarioFacebook.java
+++ app/src/main/java/com/example/fdope/tresb/VentanaUsuarioFacebook.java
@@ -2,6 +2,8 @@ package com.example.fdope.tresb;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +23,21 @@ public class VentanaUsuarioFacebook extends DialogFragment {
     private Button ingresar,salir;
     private InfoPostDialog mCallBack;
     private String nombre;
+    private TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
 
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            // check Fields For Empty Values
+            checkFieldsForEmptyValues();
+        }
+    };
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.ventana_usuarioface, container, false);
@@ -32,14 +48,15 @@ public class VentanaUsuarioFacebook extends DialogFragment {
         username = (TextView)rootView.findViewById(R.id.usuarioFacebook);
         ingresar = (Button) rootView.findViewById(R.id.botonIngresar);
         salir = (Button)rootView.findViewById(R.id.bye) ;
+        username.getText().toString();
+        username.addTextChangedListener(mTextWatcher);
+        checkFieldsForEmptyValues();
+
 
         ingresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                nombre = username.getText().toString();
-                if (!nombre.equals(""))
-                    mCallBack.postIngresarUsuarioFacebook(nombre);
+                    mCallBack.postIngresarUsuarioFacebook(username.getText().toString());
             }
         });
 
@@ -51,5 +68,15 @@ public class VentanaUsuarioFacebook extends DialogFragment {
             }
         });
         return rootView;
+    }
+
+    public void checkFieldsForEmptyValues() {
+        String nombre = this.username.getText().toString();
+
+        if (nombre.equals("")) {
+            ingresar.setEnabled(false);
+        } else {
+            ingresar.setEnabled(true);
+        }
     }
 }
